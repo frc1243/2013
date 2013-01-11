d3bb95fcd075b4558185ba803853e0bb54bcc7d1
rdennings <rodney.dennings@gmail.com>
2013-01-11 08:00:46 -0800

    Main file for the robot code
    I started to make the shooter code so I can make the error checking and error display. Should be done with that by Sunday night. If any new guys want to learn how to do the drive train (very easy, good for beginners) Talk to me (Rodney) or Andrew.  My E-mail is rodney.dennings@gmail.com

---
+++ MyRobot.cpp
@@ -0,0 +1,79 @@
+#include "WPILib.h"
+#include<cmath>
+
+//The following will be changed manually through the code
+
+float shoot_goal= .7;
+
+
+
+//===================================================================
+
+//The following will be modified by the code itself, but does not contribute to physical outputs
+
+
+int shoot_error_count= 0;
+bool shoot_encode_error= false;
+bool shoot_enable= false;
+float shooter_dif= 0;
+
+
+//==================================================================
+
+
+
+
+class RobotDemo : public SimpleRobot                //Here we are defining all of the hardware
+{
+	Joystick stick,stick1,stick2,stick3;        //Joysticks on the driverstation, left to right
+	Victor shooter;				    //This is the controller for the different motors
+	Encoder shoot_motor;
+	PCVideoServer *pc;
+	DriverStation *ds;			    //defines the space on the driver station that we can use to display data
+
+
+public:
+	RobotDemo(void):
+		stick(1)
+		stick1(2)
+		stick2(3)
+		stick3(4)
+		
+		shooter(1)
+
+
+
+	void Autonomous(void)                     //Initiate auto code. This is the code that will be executed at the start of a match 
+	{
+
+
+	}
+	
+	
+
+	void OperatorControl(void)		//Initiate Tele-Op code. Here is the 2 minutes after auto
+	{
+		
+		if(shoot_enable == true)
+		{
+			shooter.SetSpeed(.5)
+		}	
+
+
+		
+		shooter_dif = shoot_goal - shoot_motor.get()
+	
+		if(shoot_diff != 0)
+		{
+			
+		}
+
+		
+
+
+
+	}
+
+};
+
+START_ROBOT_CLASS(RobotDemo);
