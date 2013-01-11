8f0ac141e1cf01d2913751b19e059f17686ad47f
rdennings <rodney.dennings@gmail.com>
2013-01-11 15:13:27 -0500

    Update MyRobot.cpp

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -1,35 +1,32 @@
 #include "WPILib.h"
 #include<cmath>
 
+//==================================================================
+
 //The following will be changed manually through the code
 
 float shoot_goal= .7;
 
 
-
-//===================================================================
+//==================================================================
 
 //The following will be modified by the code itself, but does not contribute to physical outputs
-
 
 int shoot_error_count= 0;
 bool shoot_encode_error= false;
 bool shoot_enable= false;
-float shooter_dif= 0;
-
+float shoot_diff= 0;
 
 //==================================================================
 
 
-
-
-class RobotDemo : public SimpleRobot                //Here we are defining all of the hardware
+class RobotDemo : public SimpleRobot          //Here we are defining all of the hardware
 {
-	Joystick stick,stick1,stick2,stick3;        //Joysticks on the driverstation, left to right
-	Victor shooter;				    //This is the controller for the different motors
+	Joystick stick,stick1,stick2,stick3;      //Joysticks on the driverstation, left to right
+	Victor shooter;				    		  //This is the controller for the different motors
 	Encoder shoot_motor;
 	PCVideoServer *pc;
-	DriverStation *ds;			    //defines the space on the driver station that we can use to display data
+	DriverStation *ds;			    		  //defines the space on the driver station that we can use to display data
 
 
 public:
@@ -40,26 +37,32 @@ public:
 		stick3(4),
 		
 		shooter(1),
-
+		
+		shoot_motor(1,2,true),
+		
+		
+		ds(DriverStation::GetInstance())
+	{
+		shoot_motor.Start();
+	}
 	void Autonomous(void)                     //Initiate auto code. This is the code that will be executed at the start of a match 
 	{
 
 
 	}
 	
-	
 
-	void OperatorControl(void)		//Initiate Tele-Op code. Here is the 2 minutes after auto
+	void OperatorControl(void)				  //Initiate Tele-Op code. Here is the 2 minutes after auto
 	{
 		
 		if(shoot_enable == true)
 		{
-			shooter.SetSpeed(.5)
+			shooter.SetSpeed(.5);
 		}	
 
 
 		
-		shooter_dif = shoot_goal - shoot_motor.get()
+		shoot_diff = shoot_goal - shoot_motor.Get();
 	
 		if(shoot_diff != 0)
 		{
@@ -67,9 +70,6 @@ public:
 		}
 
 		
-
-
-
 	}
 
 };
